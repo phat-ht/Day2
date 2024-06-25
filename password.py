623251a7cbb2e06e3f45769ef5b9493bc28ec799
Thinh Phat <153502922+phat-ht@users.noreply.github.com>
2024-06-26 03:09:51 +0700

    test1

---
+++ password.py
@@ -0,0 +1,20 @@
+def main():
+ 
+  count = 0
+  while count < 5:
+    remaining_attempts = 5 - count
+    print(f"You have {remaining_attempts} attempts left!")  
+    password = input("Enter the password: ")  
+
+    if password == "toiyeubk":
+      print("You are logged in to the system!")
+      break
+    else:
+      print("Wrong password!")
+      count += 1
+
+  if count == 5:
+    print("You are kicked off the system!!!")
+
+if __name__ == "__main__":
+  main()
