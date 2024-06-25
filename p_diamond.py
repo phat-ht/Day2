c8ee68d83126d6a881c09e459647d6b33c1ebea7
Thinh Phat <153502922+phat-ht@users.noreply.github.com>
2024-06-26 03:22:43 +0700

    test3

--- p_diamond.py
+++ p_diamond.py
@@ -1,16 +1,20 @@
 def printdiamond(height):
 
-  # top half diamond
-  for i in range(1, height + 1):
-    spaces = ' ' * (height - i)
-    stars = '*' * (2 * i - 1)
-    print(spaces + stars)
+    while height <= 0:
+        print("Invalid height. Please enter a positive integer.")
+        height = int(input("Enter the height of the diamond: "))
 
-  # Bottom half diamond
-  for i in range(height - 1, 0, -1):
-    spaces = ' ' * (height - i)
-    stars = '*' * (2 * i - 1)
-    print(spaces + stars)
+    # Top half diamond
+    for i in range(1, height + 1):
+        spaces = ' ' * (height - i)
+        stars = '*' * (2 * i - 1)
+        print(spaces + stars)
+
+    # Bottom half diamond
+    for i in range(height - 1, 0, -1):
+        spaces = ' ' * (height - i)
+        stars = '*' * (2 * i - 1)
+        print(spaces + stars)
 
 # Input height
 height = int(input("Enter the height of the diamond: "))
